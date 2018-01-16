72d4ec317188733813ed63a3ac8139a48ac621e6
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-16 02:42:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians,instruments){
+  var whoPlaysWhat=[];
+  musicians.forEach(function(element,index){
+      whoPlaysWhat[index]=`${musicians[index]} plays ${instruments[index]}`;
+  })
+  return whoPlaysWhat;
+}
+
+function johnLennonFacts(facts){
+var i = 0
+  while(i<facts.length){
+    facts[i]= `${facts[i]}!!!`;
+    i++;
+    }
+  return facts;
+}
+
+
+function iLoveTheBeatles(n){
+  var array=[];
+  do{
+    array.push("I love the Beatles!");
+    n++;
+  }while(n < 15)
+  return array;
+}
