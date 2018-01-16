b8b5635e0dfa87365263d7e5756a867be03a8bc4
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-16 02:30:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function theBeatlesPlay(musicians,instruments){
   var whoPlaysWhat=[];
   musicians.forEach(function(element,index)){
       whoPlaysWhat[index]=`${musicians[index]} plays ${instruments[index]}`
-    }
   }
   return whoPlaysWhat;
 }
